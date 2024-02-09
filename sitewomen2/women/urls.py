895d16a25f7a5290eb46460f8b623c3402408e20
Orynbaiuly Temirlan <temirlanorynbai915@gmail.com>
2024-02-10 01:41:26 +0600

    Шаблоны, начало. Функции render() и render to string() !!!

--- sitewomen2/women/urls.py
+++ sitewomen2/women/urls.py
@@ -5,7 +5,8 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index),                                              # http://127.0.0.1:8000
+    path('', views.index, name='home'),                                              # http://127.0.0.1:8000
+    path('about/', views.about, name='about'),                                              # http://127.0.0.1:8000
     path('cats/<int:cat_id>/', views.categories),                       # http://127.0.0.1:8000/cats/{int}
     path('cats/<int:cat_slug>/', views.categories_by_slug),             # http://127.0.0.1:8000/cats/{slug}
     path(r"archive/<year4:year>/", views.archive),
--- sitewomen2/women/views.py
+++ sitewomen2/women/views.py
@@ -1,8 +1,15 @@
 from django.http import Http404, HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+
 
 def index(request):        # HttpRequset
-    return HttpResponse('Страница приложения women.')
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    return render(request, 'women/index.html')
+
+def about(request):
+    return render(request, 'women/about.html')
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
