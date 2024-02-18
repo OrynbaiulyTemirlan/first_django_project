990a4121c713522e56abe85ba92ea8990434e2b1
Orynbaiuly Temirlan <temirlanorynbai915@gmail.com>
2024-02-19 01:40:13 +0600

    #13. Шаблонный тег url | Уроки по Django 4

--- sitewomen2/women/urls.py
+++ sitewomen2/women/urls.py
@@ -2,12 +2,14 @@ from django.urls import path, re_path, register_converter
 from . import views
 from . import converters
 
+
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index, name='home'),                                                     # http://127.0.0.1:8000
-    path('about/', views.about, name='about'),                                              # http://127.0.0.1:8000
-    path('cats/<int:cat_id>/', views.categories),                                           # http://127.0.0.1:8000/cats/{int}
-    path('cats/<int:cat_slug>/', views.categories_by_slug),                                 # http://127.0.0.1:8000/cats/{slug}
-    path(r"archive/<year4:year>/", views.archive),
+    path('', views.index, name='home'),  # http://127.0.0.1:8000
+    path('about/', views.about, name='about'),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
