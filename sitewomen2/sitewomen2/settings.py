1171374707fe706c9cfbc2bbcad68a7ee6d4fc02
Orynbaiuly Temirlan <temirlanorynbai915@gmail.com>
2024-02-21 00:21:25 +0600

    #15. Подключение статических файлов

--- sitewomen2/sitewomen2/settings.py
+++ sitewomen2/sitewomen2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=4-#7tt%wfmjbrs7&bjzrgh(6rh9a#hsmd&t^^(_zk@c^#3k_&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
