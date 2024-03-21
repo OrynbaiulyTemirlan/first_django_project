fbec38b0f3eef3a6fadb17d21bc7c7344ae00c86
Orynbaiuly Temirlan <temirlanorynbai915@gmail.com>
2024-03-21 16:11:05 +0600

    #44. Использование форм не связанных с моделями

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
+
+from .forms import AddPostForm
 from .models import Category, TagPost, Women
 
 
@@ -42,7 +44,19 @@ def show_post(request, post_slug):
 
 
 def addpage(request):
-    return render(request, 'women/addpage.html', {'menu': menu, 'title': 'Добавление статьи'})
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = AddPostForm()
+
+    data = {
+        'menu': menu,
+        'title': 'Добавление статьи',
+        'form': form
+    }
+    return render(request, 'women/addpage.html', data)
 
 
 def contact(request):
