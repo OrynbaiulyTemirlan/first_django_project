cf90423e305b676a0acefa0f92d29260ea4d6957
Orynbaiuly Temirlan <temirlanorynbai915@gmail.com>
2024-03-30 00:21:00 +0600

    #49. Загрузка файлов с использованием моделей

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -19,6 +19,8 @@ class Women(models.Model):
                                 MinLengthValidator(5, message='Минимум 5 символов'),
                                 MaxLengthValidator(100, message='Максимум 5 символов'),
                            ])
+    photo = models.ImageField(upload_to=f'photos/%Y/%m/%d', default=None,
+                              blank=True, null=True, verbose_name='Фото')
     content = models.TextField(blank=True, verbose_name='Текст статьи')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменеия')
@@ -82,3 +84,6 @@ class Husband(models.Model):
 
     def __str__(self):
         return self.name
+
+class UploadFiles(models.Model):
+    file = models.FileField(upload_to="uploads_model")
